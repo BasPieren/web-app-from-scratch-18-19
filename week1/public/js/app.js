9c63b137887ab9ca8854be92820474fda292d961
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-02-04 16:04:33 +0100

    week1/public/js/app.js | Edit | Added array sort and extra elements

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,5 +1,10 @@
 "use strict";
 
+/* ------ SOURCES
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
+https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
+------ */
+
 var main = document.querySelector('main')
 var url = 'https://swapi.co/api/films/'
 var request = new XMLHttpRequest();
@@ -11,18 +16,27 @@ request.send()
 
 function getData() {
   var data = request.response
-  var json = JSON.parse(data)
+  var dataJSON = JSON.parse(data).results
 
-  for (var i = 0; i < json.results.length; i++) {
+  dataJSON.sort(function(a, b){
+    return parseFloat(a.episode_id) - parseFloat(b.episode_id)
+  })
+
+  for (var i = 0; i < dataJSON.length; i++) {
+    var article = document.createElement('article')
     var h2 = document.createElement('h2')
+    var h3 = document.createElement('h3')
     var p = document.createElement('p')
 
-    h2.textContent = json.results[i].title
-    p.textContent = json.results[i].opening_crawl
+    h2.textContent = dataJSON[i].title
+    h3.textContent = 'Episode ' + dataJSON[i].episode_id
+    p.textContent = dataJSON[i].opening_crawl
 
-    main.appendChild(h2)
-    main.appendChild(p)
+    main.appendChild(article)
+    article.appendChild(h3)
+    article.appendChild(h2)
+    article.appendChild(p)
   }
 
-  console.log(json.results)
+  console.log(dataJSON)
 }
