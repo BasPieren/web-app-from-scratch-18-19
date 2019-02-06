f0c090fddae36f878d8fe343445e631c12d71043
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-02-06 12:35:46 +0100

    public/js/app.js | Edit | Small variable style change and removed parseFloat

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -6,9 +6,10 @@ https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-object
 ------ */
 
 (function(){
-  var main = document.querySelector('main')
-  var url = 'https://swapi.co/api/films/'
-  var request = new XMLHttpRequest();
+
+  var main = document.querySelector('main'),
+      url = 'https://swapi.co/api/films/',
+      request = new XMLHttpRequest();
 
   request.open('GET', url)
   request.onload = getData
@@ -16,18 +17,18 @@ https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-object
   request.send()
 
   function getData() {
-    var data = request.response
-    var dataJSON = JSON.parse(data).results
+    var data = request.response,
+        dataJSON = JSON.parse(data).results
 
     dataJSON.sort(function(a, b){
-      return parseFloat(a.episode_id) - parseFloat(b.episode_id)
+      return (a.episode_id) - (b.episode_id)
     })
 
     for (var i = 0; i < dataJSON.length; i++) {
-      var article = document.createElement('article')
-      var h2 = document.createElement('h2')
-      var h3 = document.createElement('h3')
-      var p = document.createElement('p')
+      var article = document.createElement('article'),
+          h2 = document.createElement('h2'),
+          h3 = document.createElement('h3'),
+          p = document.createElement('p')
 
       h2.textContent = dataJSON[i].title
       h3.textContent = 'Episode ' + dataJSON[i].episode_id
@@ -42,4 +43,5 @@ https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-object
 
     console.log(dataJSON)
   }
+
 })();
