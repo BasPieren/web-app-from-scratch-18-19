4dd0821ea13fcf69b62cff777b83ec7aa258338c
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-02-08 16:31:48 +0100

    public/js/app.js | Edit | Refactored code

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -6,71 +6,41 @@ https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-object
 https://codepen.io/joostf/pen/OQxpxx
 ------ */
 
-(() => {
-
-  const main = document.querySelector('main'),
-        url = 'https://swapi.co/api/films/'
-        // request = new XMLHttpRequest()
-
-  function getData() {
-    fetch(url)
-      .then(respone => {
-        return respone.json()
-      })
-      .then(data => {
-        renderData(data)
-      })
-      .catch(error => console.error(error))
-  }
-
-  getData();
-
-  // let getData = new Promise((resolve, reject) => {
-  //
-  // request.open('GET', url, true)
-  // request.addEventListener("load", requestData)
-  //
-  // function requestData() {
-  //     if (request.status >= 200 && request.status < 400) {
-  //       const data = JSON.parse(request.responseText)
-  //       resolve(data)
-  //     } else {
-  //       reject(error)
-  //     }
-  //   }
-  //   request.onerror = (e) => {
-  //     resolve(e)
-  //   }
-  //
-  //   request.send()
-  // })
-  //
-  // getData
-  //   .then(data => renderData(data))
-  //   .catch(error => console.error(error))
-
-  function renderData(e) {
-
-    e.results.sort((a, b) => {
-      return (a.episode_id) - (b.episode_id)
+function getData() {
+  fetch('https://swapi.co/api/films/')
+    .then(respone => {
+      return respone.json()
     })
-
-    e.results.forEach((a) => {
-      const article = document.createElement('article'),
-            h2 = document.createElement('h2'),
-            h3 = document.createElement('h3'),
-            p = document.createElement('p')
-
-      h2.textContent = a.title
-      h3.textContent = 'Episode ' + a.episode_id
-      p.textContent = a.opening_crawl
-
-      main.appendChild(article)
-
-      article.appendChild(h3)
-      article.appendChild(h2)
-      article.appendChild(p)
+    .then(data => {
+      renderData(data)
     })
-  }
+    .catch(error => console.error(error))
+}
 
-})()
+getData();
+
+function renderData(e) {
+
+  const main = document.querySelector('main')
+
+  e.results.sort((a, b) => {
+    return (a.episode_id) - (b.episode_id)
+  })
+
+  e.results.forEach((a) => {
+    const article = document.createElement('article'),
+          h2 = document.createElement('h2'),
+          h3 = document.createElement('h3'),
+          p = document.createElement('p')
+
+    h2.textContent = a.title
+    h3.textContent = 'Episode ' + a.episode_id
+    p.textContent = a.opening_crawl
+
+    main.appendChild(article)
+
+    article.appendChild(h3)
+    article.appendChild(h2)
+    article.appendChild(p)
+  })
+}
