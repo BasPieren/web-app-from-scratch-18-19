97cef418f11297b6f467289706784e169adca147
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-02-15 09:24:54 +0100

    public/js/app.js | Edit | Started work on routing (unfinished)

--- week2/public/js/app.js
+++ week2/public/js/app.js
@@ -15,15 +15,15 @@ function getData() {
       return response.json()
     })
     .then(data => {
+      // Routing
       routie({
         'allMovies': () => {
           renderData(data)
         },
-        'detailPage': () => {
-          detailPage(data)
+        'movie/:episode': (episode) => {
+          detailPage(episode, data)
         }
       })
-      // filterData(data)
     })
     .catch(error => console.error(error))
 }
@@ -31,9 +31,8 @@ getData()
 
 // Render overview page
 function renderData(e) {
-  console.log(e)
-  let newObj = filterData(e),
-      directives = {
+  let filteredData = filterData(e),
+      dataDirectives = {
         episode_id: {
           text: function(params) { // Arrow function doesn't work?
             return params.value + this.episode_id
@@ -44,38 +43,62 @@ function renderData(e) {
             return params.value
           },
           href: function() { // Arrow function doesn't work?
-            return this.detail_page
+            return '#movie/' + this.detail_page
           }
         }
       }
 
+  // To do: Give each template its own function
   const main = document.querySelector('main'),
         template = `
         <article>
-          <h3 class="episode_id">Episode </h2>
-          <h2 class="title"></h3>
+          <h3 class="episode_id">Episode </h3>
+          <h2 class="title"></h2>
           <p class="opening_crawl"></p>
           <a class="detail_page">Details</a>
         </article>
         `
 
-  newObj.sort((a, b) => {
-    return (a.episode_id) - (b.episode_id)
-  })
-
   main.innerHTML = template
 
-  Transparency.render(main, newObj, directives)
+  Transparency.render(main, filteredData, dataDirectives)
 }
 
-function detailPage(e) {
-  console.log('data')
+// Render detail page
+function detailPage(episode, data) {
+  let newObj = filterData(data),
+      findMatch = newObj.find(d => d.episode_id == episode),
+      dataDirectives = {
+        episode_id: {
+          text: function(params) { // Arrow function doesn't work?
+            return params.value + this.episode_id
+          }
+        }
+      }
+
+  // To do: Give each template its own function
+  let body = document.querySelector('body')
+  let main2 = document.querySelector('main'),
+        template = `
+        <article>
+          <h3 class="episode_id">Episode </h3>
+          <h2 class="title"></h2>
+          <p class="opening_crawl"></p>
+        </article>
+        `
+    main2.innerHTML = ''
+    main2.innerHTML = template
+
+    // To do: Body container needs to become main
+    Transparency.render(body, findMatch, dataDirectives)
+
 }
 
+// Filter the data
 function filterData(e) {
   let allMovies = e.results
 
-  let dataFiltered = allMovies.map(a =>{
+  let dataFiltered = allMovies.map(a => {
     return{
       title: a.title,
       episode_id: a.episode_id,
@@ -83,6 +106,10 @@ function filterData(e) {
       opening_crawl: a.opening_crawl,
       detail_page: a.episode_id
     }
+  })
+
+  dataFiltered.sort((a, b) => {
+    return (a.episode_id) - (b.episode_id)
   })
 
   return dataFiltered
