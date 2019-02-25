0ec63e3a81a3f84c922c3d6f1f148afc031fac1c
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-02-25 13:08:45 +0100

    public/js/modules/template.js | Edit | Removed unnecessary export of functions

--- week3/public/js/modules/template.js
+++ week3/public/js/modules/template.js
@@ -1,4 +1,3 @@
-// Render overview page
 export function overviewPage(e) {
   const mainOverview = document.querySelector('main')
   let filteredData = e,
@@ -21,23 +20,7 @@ export function overviewPage(e) {
   overviewTemplate(mainOverview, filteredData, dataDirectives)
 }
 
-// Render detail page
-export function detailPage(episode, data) {
-  const mainDetail = document.querySelector('main')
-  let findMatch = data.find(d => d.episode_id == episode),
-      dataDirectives = {
-        episode_id: {
-          text: function(params) { // Arrow function doesn't work?
-            return params.value + this.episode_id
-          }
-        }
-      }
-
-  detailTemplate(mainDetail, findMatch, dataDirectives)
-}
-
-// Overview page template
-export function overviewTemplate(container, data, dataDir) {
+function overviewTemplate(container, data, dataDir) {
   const article = document.createElement('article')
   let overviewTemplate = `
     <h3 class="episode_id">Episode </h3>
@@ -53,8 +36,21 @@ export function overviewTemplate(container, data, dataDir) {
   Transparency.render(container, data, dataDir)
 }
 
-// Detail page template
-export function detailTemplate(container, data, dataDir) {
+export function detailPage(episode, data) {
+  const mainDetail = document.querySelector('main')
+  let findMatch = data.find(d => d.episode_id == episode),
+      dataDirectives = {
+        episode_id: {
+          text: function(params) { // Arrow function doesn't work?
+            return params.value + this.episode_id
+          }
+        }
+      }
+
+  detailTemplate(mainDetail, findMatch, dataDirectives)
+}
+
+function detailTemplate(container, data, dataDir) {
   const article = document.createElement('article')
   let detailTemplate = `
     <h3 class="episode_id">Episode </h3>
