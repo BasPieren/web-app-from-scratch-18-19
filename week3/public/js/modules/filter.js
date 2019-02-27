84688758dec2cbb8c672bc7c6a701f1eac3da240
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-02-27 15:10:10 +0100

    public/js/modules/filter.js | Edit | Added extra data to the filter

--- week3/public/js/modules/filter.js
+++ week3/public/js/modules/filter.js
@@ -6,10 +6,11 @@ export function filterData(e) {
           episode_id: a.episode_id,
           release_date: a.release_date,
           opening_crawl: a.opening_crawl,
+          release_date: a.release_date,
+          producer: a.producer,
           detail_page: a.episode_id
         }
       })
-
   dataFiltered.sort((a, b) => {
     return (a.episode_id) - (b.episode_id)
   })
